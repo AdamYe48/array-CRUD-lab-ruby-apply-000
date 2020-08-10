bdb7074844ff257d7c4e17f19513a2417bb23507
Adam Ye <adamye48@gmail.com>
2020-08-10 18:55:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -28,8 +28,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  g = ["i", "am", "really", "learning", "arrays!"]
-  g["i"]
+  g = ["wow", "am", "really", "learning", "arrays!"]
+  g["wow"]
 end
 
 def retrieve_last_element_from_array(array)
